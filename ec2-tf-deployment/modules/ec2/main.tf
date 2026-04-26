5afa9fd58f7e2a5c64da730deb6665c1cb762c0d
ruwendraben <ruwendraben@gmail.com>
2026-04-26 23:40:18 +0530

    enable user_data replace on change

--- ec2-tf-deployment/modules/ec2/main.tf
+++ ec2-tf-deployment/modules/ec2/main.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "sob" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  iam_instance_profile   = var.iam_instance_profile
-  vpc_security_group_ids = [var.security_group_id]
-  key_name               = var.key_pair_name != "" ? var.key_pair_name : null
-  user_data              = var.user_data
+  ami                          = var.ami_id
+  instance_type                = var.instance_type
+  iam_instance_profile         = var.iam_instance_profile
+  vpc_security_group_ids       = [var.security_group_id]
+  key_name                     = var.key_pair_name != "" ? var.key_pair_name : null
+  user_data                    = var.user_data
+  user_data_replace_on_change  = true
 
   tags = {
     Name        = var.instance_name
