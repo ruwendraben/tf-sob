7b0a31f1afccf256c2ce017fbe7b45cc8f72c49e
ruwendraben <ruwendraben@gmail.com>
2026-04-28 13:32:40 +0530

    Adding shell execution policy for dev-user IAM user

--- ec2-tf-deployment/modules/iam/main.tf
+++ ec2-tf-deployment/modules/iam/main.tf
@@ -21,9 +21,9 @@ resource "aws_iam_role_policy_attachment" "ecr_readonly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_readonly" {
+resource "aws_iam_role_policy_attachment" "ssm_managed_core" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy" "s3_scoped" {
